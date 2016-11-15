7e0eef48ed3971f85814504ff2cd847515ade622
xiaoxiaofengzi <737227922@qq.com>
2016-11-15 20:39:19 +0800

    test start

--- src/pcep_msg_create_v2.erl
+++ src/pcep_msg_create_v2.erl
@@ -15,6 +15,7 @@
 ls_report_nodes_msg_creating(SwitchNum) ->
   LS_Report_Nodes_Msgs_1 = linc_pcep_config:for(1,SwitchNum,fun(I) ->
     LS_Report_Node_Msgs_1 = pcep_msg_create:ls_report_node_msg_creating(I-1),
+    io:format("LS_Report_Node_Msg is ~p~n",[LS_Report_Node_Msgs_1]),
     element(2,LS_Report_Node_Msgs_1)
   end),
   list_to_binary(LS_Report_Nodes_Msgs_1).
